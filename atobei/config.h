00b4728f66c8f28624c5a7c966ad9b00174c06f8
ouser555 <95702400+ouser555@users.noreply.github.com>
2022-05-04 18:02:31 +0800

    Add files via upload
    修改VENDOR_ID 使remap-keys不會與其他鍵盤衝突

--- atobei/config.h
+++ atobei/config.h
@@ -6,7 +6,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
+#define VENDOR_ID    0xAAED
 #define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
 #define MANUFACTURER ouser555
